371de7156669722c37a84bb2209d59292d0640f4
Jatin Bansal <134193834+JatinBansal09@users.noreply.github.com>
2026-07-12 18:20:20 +0530

    Re-add settings_test.py for pytest

---
+++ my-app/backend/backend/settings_test.py
@@ -0,0 +1,14 @@
+from .settings_prod import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
+}
